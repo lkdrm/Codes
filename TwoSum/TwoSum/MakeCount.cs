3d8bf9d4f6058870d7333ea4c3d0f3e532bfd159
LkDrm(Vitalik) <larik115@gmail.com>
2023-11-05 14:45:37 +0100

    Upgrade, remove ugly methods
    Signed-off-by: LkDrm(Vitalik) <larik115@gmail.com>

--- TwoSum/TwoSum/MakeCount.cs
+++ TwoSum/TwoSum/MakeCount.cs
@@ -11,61 +11,30 @@
         public List<int> Numbers = new();
 
         /// <summary>
-        /// Sum of two numbers
-        /// </summary>
-        private int SumOfNumbers { get; set; }
-
-        /// <summary>
-        /// Will be used to find a index
-        /// </summary>
-        private int UsedFirstNumber = 0;
-
-        /// <summary>
-        /// Will be used to find a index
-        /// </summary>
-        private int UsedSecondNumber = 0;
-
-        /// <summary>
         /// Will count numbers and return from list current target number
         /// </summary>
         /// <param name="targetNumber">Which number must be returned</param>
-        public string FindTargetNumber(int targetNumber)
+        public int[] FindTargetNumber(int targetNumber)
         {
             for (int i = 0; i < Numbers.Count; i++)
             {
                 for (int j = 1; j < Numbers.Count; j++)
                 {
-                    if (Numbers.Count > 2 && Numbers[i] == Numbers[j])
+                    if ((Numbers[i] + Numbers[j]).Equals(targetNumber))
                     {
-                        continue;
-                    }
-
-                    SumOfNumbers = Numbers[i] + Numbers[j];
-                    UsedFirstNumber = Numbers[i];
-                    UsedSecondNumber = Numbers[j];
-
-                    if (SumOfNumbers == targetNumber)
-                    {
-                        break;
+                        if (i == j)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return new int[] { i, j };
+                        }
                     }
                 }
-
-                if (SumOfNumbers == targetNumber)
-                {
-                    SumOfNumbers = 0;
-                    break;
-                }
             }
 
-            if (Numbers.Count < 3)
-            {
-                return "[0,1]";
-
-            }
-            else
-            {
-                return $"[{Numbers.IndexOf(UsedFirstNumber)},{Numbers.IndexOf(UsedSecondNumber)}]";
-            }
+            return new int[] { -1, -1 };
         }
 
         /// <summary>
@@ -74,42 +43,27 @@
         /// <param name="numbers">Array of numbers</param>
         /// <param name="targetNumber">Which number must be returned</param>
         /// <returns>Indexes which was used to find a sum of target number</returns>
-        public string FindTargetNumber(int[] numbers, int targetNumber)
+        public int[] FindTargetNumber(int[] numbers, int targetNumber)
         {
             for (int i = 0; i < numbers.Length; i++)
             {
                 for (int j = 1; j < numbers.Length; j++)
                 {
-                    if (numbers.Length > 2 && numbers[i] == numbers[j])
+                    if ((numbers[i] + numbers[j]).Equals(targetNumber))
                     {
-                        continue;
-                    }
-
-                    SumOfNumbers = numbers[i] + numbers[j];
-                    UsedFirstNumber = numbers[i];
-                    UsedSecondNumber = numbers[j];
-
-                    if (SumOfNumbers == targetNumber)
-                    {
-                        break;
+                        if (i == j)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return new int[] { i, j };
+                        }
                     }
                 }
-
-                if (SumOfNumbers == targetNumber)
-                {
-                    SumOfNumbers = 0;
-                    break;
-                }
             }
 
-            if (numbers.Length < 3)
-            {
-                return "[0,1]";
-            }
-            else
-            {
-                return $"[{Array.FindIndex(numbers, n => n.Equals(UsedFirstNumber))},{Array.FindIndex(numbers, n => n.Equals(UsedSecondNumber))}]";
-            }
+            return new int[] { -1, -1 };
         }
     }
 }
