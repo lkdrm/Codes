6b4a6efc01af8ff3b919cfba26f703de1f06330a
LkDrm(Vitalik) <larik115@gmail.com>
2023-11-09 22:58:22 +0100

    Ready another code - AddTwoNumbers
    Signed-off-by: LkDrm(Vitalik) <larik115@gmail.com>

---
+++ AddTwoNumbers/AddTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using AddTwoNumbers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var mC = new MakeCount();
+
+        Console.WriteLine("Write random numbers, it will make an list and count it\nWill return reverse list\nFirst array:");
+        var firstList = Console.ReadLine().ToArray();
+        Console.WriteLine("Second array:");
+        var secondList = Console.ReadLine().ToArray();
+        Console.Write("[ ");
+        foreach (var item in mC.TakeAndCount(firstList, secondList))
+        {
+            Console.Write(item + " ");
+        }
+        Console.Write("]");
+    }
+}
